2a78dfe1516e0bc2c11feeba6e27e572ff0a4aae
augusstt06 <augusstt06@gmail.com>
2024-12-15 14:58:21 +0900

    add: development roadmap

--- src/app/components/pages/RoadMap.tsx
+++ src/app/components/pages/RoadMap.tsx
@@ -1,7 +1,19 @@
+type TRoadMapTitle = {
+  title: string
+}
 export default function RoadMap() {
   return (
     <div className="text-white h-screen bg-red-100">
-      <h1>RoadMap</h1>
+      <RoadMapTitle title="Development Roadmap" />
+    </div>
+  )
+}
+
+function RoadMapTitle(props: TRoadMapTitle) {
+  const { title } = props
+  return (
+    <div className="row-flex justify-center xl:justify-start items-center">
+      <h1 className="text-4xl px-16 py-4">{title}</h1>
     </div>
   )
 }
