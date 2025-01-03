99730568517a94eb028be1437e2600bce340df2e
augusstt06 <augusstt06@gmail.com>
2025-01-03 15:22:30 +0900

    fix: roadmap

--- src/components/pages/RoadMap.tsx
+++ src/components/pages/RoadMap.tsx
@@ -7,12 +7,16 @@ import {
   VerticalTimelineElement,
 } from 'react-vertical-timeline-component'
 
+import Link from 'next/link'
 import {
   FaExchangeAlt,
   // FaProjectDiagram,
   FaRocket,
   FaUsers,
 } from 'react-icons/fa'
+import { MdDeveloperBoard } from 'react-icons/md'
+
+import { ETHERSCAN_URL } from '@/constant'
 
 type Props = {
   roadmapRef: React.RefObject<HTMLDivElement>
@@ -23,7 +27,7 @@ type TRoadMapTitle = {
 type TRoadMapTimeline = {
   date: string
   title: string
-  content: string
+  content: React.ReactNode
   color: string
   summary: string
 }
@@ -51,23 +55,36 @@ function RoadmapComponent() {
     {
       date: 'DEC 2024',
       title: 'Kuku Coin development based on solidity',
-      content: 'Developing a website to introduce Kuku Coin',
+      content: (
+        <>
+          <p>Developing a website to introduce Kuku Coin.</p>
+          <p>Smart contract writing and web front-end development.</p>
+        </>
+      ),
       color: '#3b82f6',
-      summary: 'rocket',
+      summary: 'dev',
     },
     {
       date: 'JAN 2025',
-      title: 'Launch of Kuku Coin Website and Initial Community Building.',
-      content: 'DEX listing and airdrop event',
+      title: 'Kuku Coin officially launched.',
+      content: (
+        <>
+          <p>Smart contract testing and mainnet deployment.</p>
+
+          <p>Click the button below to check the contract address.</p>
+          <button className="bg-[#1d634c] hover:bg-[#1d4a3b] simple-transition md:text-md text-sm text-white px-4 py-2 rounded-md mt-4">
+            <Link href={ETHERSCAN_URL}>Contract Address</Link>
+          </button>
+        </>
+      ),
       color: '#10b981',
-      summary: 'exchange',
+      summary: 'rocket',
     },
     // FIXME: 로드맵 워딩 생각하기
     {
       date: 'FEB 2025',
-      title: 'Enhancement of user base and expansion',
-      content:
-        'Listing on a centralized exchange and release of NFT collection',
+      title: 'Collect community feedback and improve services.',
+      content: 'Gathering feedback from the community to enhance the service.',
       color: '#f59e0b',
       summary: 'users',
     },
@@ -93,6 +110,8 @@ function RoadMapTimelineElement(props: TRoadMapTimeline) {
         return <FaExchangeAlt />
       case 'users':
         return <FaUsers />
+      case 'dev':
+        return <MdDeveloperBoard />
     }
   }
   return (
@@ -104,7 +123,7 @@ function RoadMapTimelineElement(props: TRoadMapTimeline) {
       iconStyle={{ background: color, color: '#fff' }}
       icon={Icon()}
     >
-      <h3 className="vertical-timeline-element-title">{title}</h3>
+      <h3 className="vertical-timeline-element-title text-xl">{title}</h3>
       <p className="text-lg">{content}</p>
     </VerticalTimelineElement>
   )
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -5,3 +5,5 @@ export const ROADMAP_TITLE = 'Roadmap'
 export const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS
 
 export const GITBOOK_URL = 'https://augusstt-note.gitbook.io/kuku-coin'
+export const ETHERSCAN_URL =
+  'https://sepolia.etherscan.io/address/0x2A2324644334e39E2a3AE8d8EE09567A9ec733A1'
